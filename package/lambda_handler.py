5dac881339f6e68183b2507843b3771f3c81e298
Prathveer Rai <Prathveer.Rai@nike.com>
2019-06-14 23:09:38 -0700

    Updated lambda handler to publish messages and on failure too

--- package/lambda_handler.py
+++ package/lambda_handler.py
@@ -3,20 +3,23 @@ import boto3
 from io import BytesIO
 
 def lambda_handler(event,context):
-    sns = boto3.resource("sns")
-    topic = sns.Topic("arn:aws:sns:us-west-2:356227741561:MyPortfolioTopic")
-    s3 = boto3.client("s3")
+    try:
+        sns = boto3.resource("sns")
+        topic = sns.Topic("arn:aws:sns:us-west-2:356227741561:MyPortfolioTopic")
+        s3 = boto3.client("s3")
 
-    build_bucket = "portfoliobuild.prathveerrai.info"
-    portfolio_bucket = "portfolio.prathveerrai.info"
-    portfolio_zip = BytesIO()
-    s3.download_fileobj(build_bucket,'portfoliobuild.zip',portfolio_zip)
+        build_bucket = "portfoliobuild.prathveerrai.info"
+        portfolio_bucket = "portfolio.prathveerrai.info"
+        portfolio_zip = BytesIO()
+        s3.download_fileobj(build_bucket,'portfoliobuild.zip',portfolio_zip)
 
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm,"r")
-            s3.upload_fileobj(obj,portfolio_bucket,nm)
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm,"r")
+                s3.upload_fileobj(obj,portfolio_bucket,nm)
 
-    topic.publish(Subject="Portfolio Update Complete",Message="Hey Patty, completed deploying new updated portfolio")
-    return "Completed Running new deployment"
+        topic.publish(Subject="Portfolio Update Complete",Message="Hey Patty, completed deploying new updated portfolio")
+        return "Completed Running new deployment"
+    except:
+        topic.publish(Subject="Portfolio Update Failed",Message="Hey Patty, failed deploying new updated portfolio")
