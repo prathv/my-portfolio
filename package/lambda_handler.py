2ad8bd54fe2af92c63ae5ec0cc2c276936a60a11
Prathveer Rai <Prathveer.Rai@nike.com>
2019-06-14 22:55:49 -0700

    Added sns topic subscription code"

--- package/lambda_handler.py
+++ package/lambda_handler.py
@@ -3,8 +3,9 @@ import boto3
 from io import BytesIO
 
 def lambda_handler(event,context):
-    session = boto3.Session(profile_name='pattyaws')
-    s3 = session.client("s3")
+    sns = boto3.resource("sns")
+    topic = sns.Topic("arn:aws:sns:us-west-2:356227741561:MyPortfolioTopic")
+    s3 = boto3.client("s3")
 
     build_bucket = "portfoliobuild.prathveerrai.info"
     portfolio_bucket = "portfolio.prathveerrai.info"
@@ -17,4 +18,5 @@ def lambda_handler(event,context):
             obj = myzip.open(nm,"r")
             s3.upload_fileobj(obj,portfolio_bucket,nm)
 
+    topic.publish(Subject="Portfolio Update Complete",Message="Hey Patty, completed deploying new updated portfolio")
     return "Completed Running new deployment"
--- package/uploadPortfolio.py
+++ package/uploadPortfolio.py
@@ -2,8 +2,6 @@ import zipfile
 import boto3
 from io import BytesIO
 
-sns = boto3.resource("sns")
-topic = sns.Topic("arn:aws:sns:us-west-2:356227741561:MyPortfolioTopic")
 s3 = boto3.client("s3")
 
 build_bucket = "portfoliobuild.prathveerrai.info"
@@ -16,3 +14,4 @@ with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm,"r")
         s3.upload_fileobj(obj,portfolio_bucket,nm)
+
