9d3521d6441fc8ad3f4615e98c9a89f813effa03
Prathveer Rai <Prathveer.Rai@nike.com>
2019-06-16 15:50:36 -0700

    Added tests for ExampleWork and WorkBubble, Updated buildspec yml too

---
+++ package/newUploadPortfolio.py
@@ -0,0 +1,43 @@
+import zipfile
+import boto3
+from io import BytesIO
+
+
+def lambda_handler(event,context):
+    location = {
+        "bucketName" : "portfoliobuild.prathveer.info",
+        "objectKey" : "portfoliobuild.zip"
+    }
+    sns = boto3.resource("sns")
+    topic = sns.Topic("arn:aws:sns:us-west-2:356227741561:MyPortfolioTopic")
+    try:
+        job = event.get("CodePipeline.job")
+        print(job)
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+
+
+        print("Build portfolio from "+location["bucketName"])
+        s3 = boto3.client("s3")
+        build_bucket = location["bucketName"]
+        portfolio_bucket = "portfolio.prathveer.info"
+        portfolio_zip = BytesIO()
+        s3.download_fileobj(build_bucket,"portfoliobuild.zip",portfolio_zip)
+
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm,"r")
+                s3.upload_fileobj(obj,portfolio_bucket,nm)
+
+        topic.publish(Subject="Portfolio Update Complete",Message="Hey Patty, completed deploying new updated portfolio")
+        return "Completed Running new deployment"
+
+        if job:
+            codepipeline = boto3.client("codepipeline")
+            codepipeline.put_job_success_result(jobId=job["id"])
+    except:
+        topic.publish(Subject="Portfolio Update Failed",Message="Hey Patty, failed deploying new updated portfolio")
+        raise
